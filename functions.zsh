5f17f9ed7fc8a58aac671881236d24ea84011887
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-08-10 01:57:25 -0400

    Add files via upload

---
+++ functions.zsh
@@ -0,0 +1,33 @@
+
+  ______________________________________________________
+ |                                                      |
+ |                     ~{ F  N  C }~                    |
+ |                                                      |
+ | [rmk]-----------> [ remove a file with overwrite ]   |
+ | [nps]-----------> [ start nipe ]                     |
+ | [npr]-----------> [ restart nipe ]                   |
+ | [ipu]-----------> [ public ip cloudlfare]            |
+ | [ipi]-----------> [ private ip ]                     |
+ | [0f]------------> [ turn off system ]                |
+ | [r3]------------> [ restart system ]                 |
+ | [wq]------------> [ set wallpaper with properties ]  |
+ | [ee]------------> [ close zsh ]                      |
+ | [npo]-----------> [ stop nipe ]                      |
+ | [npt]-----------> [ status nipe ]                    |
+ | [ipp]-----------> [ public ip opendns ]              |
+ | [ipg]-----------> [ public ip google ]               |
+ | [ipc]-----------> [ public ip ican ]                 |
+ | [ipify]---------> [ public ip ipify ]                |
+ | [ipaws]---------> [ public ip amazon aws ]           |
+ | [allsv]---------> [ check public ip on all servers]  |
+ | [ipl]-----------> [ allsv loop ]                     |
+ | [sap]-----------> [ sudo apt update ]                |
+ | [sag]-----------> [ sudo apt upgrade ]               |
+ | [sal]-----------> [ show this ]                      |
+ | [lil]-----------> [ show relevant cli arguments ]    |
+ | [edi]-----------> [ edit zshrc ]                     |
+ | [fnc]-----------> [ edit fucntions.md ]              |
+ | [ran]-----------> [ recursively remove folders ]     |
+ | [hip]-----------> [ show htb ip address ]            |
+ |______________________________________________________|
+
